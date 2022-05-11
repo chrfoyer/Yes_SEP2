6668780ae480d3377890256029022c0e7a7558b7
KrunoNeric <91593987+KrunoNeric@users.noreply.github.com>
2022-05-11 10:15:15 +0200

    view_0.02

---
+++ client/src/view/LoginVeiwController.java
@@ -0,0 +1,23 @@
+package view;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class LoginVeiwController extends ViewController
+{
+  public TextField username;
+  public TextField password;
+
+  @Override protected void init()
+  {
+
+  }
+
+  public void login(ActionEvent actionEvent)
+  {
+  }
+
+  public void signup(ActionEvent actionEvent)
+  {
+  }
+}
