17e74e708fc6349f8e2865a56ee3e332d6797d2d
chrfoyer <chrfoyer@gmail.com>
2022-05-11 13:56:22 +0200

    Inventory, admin
    Fxml

--- client/src/view/AdminViewController.java
+++ client/src/view/AdminViewController.java
@@ -8,18 +8,26 @@ import javafx.scene.control.TextField;
 public class AdminViewController {
 
 
-  @FXML public TextField registeredUsers;
-  @FXML public TextField totalGames;
-  @FXML public TextField rentedGames;
-  @FXML public TextField recentGame;
-  @FXML public Label errorLabel;
+  @FXML
+  public TextField registeredUsers;
+  @FXML
+  public TextField totalGames;
+  @FXML
+  public TextField rentedGames;
+  @FXML
+  public TextField recentGame;
+  @FXML
+  public Label errorLabel;
 
-  @FXML public void manageUsers(ActionEvent actionEvent) {
+  @FXML
+  public void manageUsers(ActionEvent actionEvent) {
   }
 
-  @FXML public void transactions(ActionEvent actionEvent) {
+  @FXML
+  public void transactions(ActionEvent actionEvent) {
   }
 
-  @FXML public void manageGames(ActionEvent actionEvent) {
+  @FXML
+  public void manageGames(ActionEvent actionEvent) {
   }
 }
---
+++ client/src/view/InventoryViewController.java
@@ -0,0 +1,31 @@
+package view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class InventoryViewController {
+  @FXML
+  public TableView table;
+  @FXML
+  public TableColumn nameColumn;
+  @FXML
+  public TableColumn timeColumn;
+  @FXML
+  public TableColumn dateAddedColumn;
+
+  @FXML
+  public void add(ActionEvent actionEvent) {
+  }
+
+  @FXML
+  public void edit(ActionEvent actionEvent) {
+
+  }
+
+  @FXML
+  public void remove(ActionEvent actionEvent) {
+    // TODO: 11/05/2022 Add confirmation window with the name of the game.
+  }
+}
