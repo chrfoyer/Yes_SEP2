e8857b3cf4802dc826b9ac6fb487d9a729d479d5
Raedrim <43709947+Raedrim@users.noreply.github.com>
2022-05-12 09:35:35 +0200

    bob

--- client/src/MyApplication.java
+++ client/src/MyApplication.java
@@ -1,4 +1,6 @@
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 import mediator.RemoteModel;
 import view.ViewHandler;
@@ -21,15 +23,18 @@ public class MyApplication extends Application
       catch (Exception ex)
       {
         ex.printStackTrace();
+        Alert alert = new Alert(Alert.AlertType.ERROR,
+            "Server connection not detected, please restart server");
+        Platform.runLater(alert::showAndWait);
       }
       // The model is now responsible for creating the client object
 
       ViewModelFactory viewModelFactory = new ViewModelFactory(server);
       ViewHandler view = new ViewHandler(viewModelFactory);
 
-//      RmiClient rmiClient = new RmiClient();
-//      rmiClient.setUsername("BobTest");
-//      rmiClient.send("I'm locked in");
+      //      RmiClient rmiClient = new RmiClient();
+      //      rmiClient.setUsername("BobTest");
+      //      rmiClient.send("I'm locked in");
 
       view.start(primaryStage);
     }
--- client/src/viewmodel/SignupViewModel.java
+++ client/src/viewmodel/SignupViewModel.java
@@ -141,18 +141,22 @@ public class SignupViewModel
       if (!passwordProperty.get().equals(confirmProperty.get()))
         throw new IllegalArgumentException(
             "Passwords and confirmation have to match!");
-
-      //todo no work
       LocalDate dob = dobProperty.get();
       Period age = Period.between(dob, LocalDate.now());
-      /*
       if (age.getYears() < 13)
       {
         dobProperty.set(null);
         throw new IllegalArgumentException(
             "User has to be at least 13 years old!");
       }
-       */
+      if (usernameProperty.get().length() < 5)
+        throw new IllegalArgumentException(
+            "Username has to be at least 5 characters!");
+      if (passwordProperty.get().length() < 7)
+        throw new IllegalArgumentException(
+            "password has to be at least 7 characters!");
+      if (!emailProperty.get().contains("@"))
+        throw new IllegalArgumentException("Email not in correct format!");
       User user = new User(usernameProperty.get(), passwordProperty.get(),
           emailProperty.get(), addressProperty.get(), nameProperty.getName(),
           dob);
