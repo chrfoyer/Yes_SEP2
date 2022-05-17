99759c8a1ecc513bf5737af7b27865072f646593
chrfoyer <chrfoyer@gmail.com>
2022-05-17 23:47:09 +0200

    Read single
    Must go faster

--- SQL_Test/src/GameDAO_Impl.java
+++ SQL_Test/src/GameDAO_Impl.java
@@ -1,17 +1,23 @@
 import java.sql.*;
 
-public class GameDAO_Impl implements GameDAO {
+public class GameDAO_Impl implements GameDAO
+{
   private static GameDAO_Impl instance;
   private static Object lock = new Object();
 
-  private GameDAO_Impl() throws SQLException {
+  private GameDAO_Impl() throws SQLException
+  {
     DriverManager.registerDriver(new org.postgresql.Driver());
   }
 
-  public static GameDAO_Impl getInstance() throws SQLException {
-    if (instance == null) {
-      synchronized (lock) {
-        if (instance == null) {
+  public static GameDAO_Impl getInstance() throws SQLException
+  {
+    if (instance == null)
+    {
+      synchronized (lock)
+      {
+        if (instance == null)
+        {
           instance = new GameDAO_Impl();
         }
       }
@@ -19,20 +25,23 @@ public class GameDAO_Impl implements GameDAO {
     return instance;
   }
 
-  private Connection getConnection() throws SQLException {
+  private Connection getConnection() throws SQLException
+  {
     return DriverManager.getConnection(
-            "jdbc:postgresql://localhost:5432/postgres?currentSchema=gametest",
-            "postgres", "VIASEP2Y");
+        "jdbc:postgresql://localhost:5432/postgres?currentSchema=gametest"
+        //            , "postgres", "VIASEP2Y"
+    );
   }
 
-  @Override
-  public Game create(String name, String producer, String console,
-                     String esrb) throws SQLException {
-    try (Connection connection = getConnection()) {
+  @Override public Game create(String name, String producer, String console,
+      String esrb) throws SQLException
+  {
+    try (Connection connection = getConnection())
+    {
       PreparedStatement statement = connection.prepareStatement(
-              "INSERT INTO games" +
-                      "(name, producer, console, rented, daysLeft, review, esrb, dateAdded) " +
-                      "VALUES (?, ?, ?, false, 14, 3.0, ?, CURRENT_DATE);");
+          "INSERT INTO games"
+              + "(name, producer, console, rented, daysLeft, review, esrb, dateAdded) "
+              + "VALUES (?, ?, ?, false, 14, 3.0, ?, CURRENT_DATE);");
       statement.setString(1, name);
       statement.setString(2, producer);
       statement.setString(3, console);
@@ -42,42 +51,52 @@ public class GameDAO_Impl implements GameDAO {
     }
   }
 
-  @Override
-  public void readById(int id) throws SQLException {
-    try (Connection connection = getConnection()) {
+  @Override public Game readById(int id) throws SQLException
+  {
+    Game readGame = null;
+    try (Connection connection = getConnection())
+    {
       Statement st = connection.createStatement();
       ResultSet rs = st.executeQuery(
-              "SELECT *" +
-                      "FROM games" +
-                      "WHERE id = '1'");
-      while (rs.next()) {
-        System.out.println("Returned for you");
-        System.out.println(rs.getString(1));
+          "SELECT * " + "FROM games " + "WHERE id = 1;");
+      while (rs.next())
+      {
+        System.out.println("Id: " + rs.getString("id"));
+        String name = rs.getString("name");
+        String producer = rs.getString("producer");
+        String console = rs.getString("console");
+        String esrb = rs.getString("esrb");
+        readGame = new Game(name, producer, console, esrb);
+
       }
       rs.close();
       st.close();
     }
+    return readGame;
   }
 
-
-  @Override
-  public void update(Game game) throws SQLException {
-
-  }
-
-  @Override
-  public void delete(Game game) throws SQLException {
+  @Override public void update(Game game) throws SQLException
+  {
 
   }
 
-  public static void main(String[] args) {
-    try {
+  @Override public void delete(Game game) throws SQLException
+  {
+
+  }
+
+  public static void main(String[] args)
+  {
+    try
+    {
       GameDAO_Impl testThing = GameDAO_Impl.getInstance();
-//      testThing.create("Cod", "Activision", "Xbox", "E");
-//      testThing.create("Battlefield", "Dice", "PlayStation", "M");
-//      testThing.create("Rust", "FacePunch", "PC", "M");
+      //      testThing.create("Cod", "Activision", "Xbox", "E");
+      //      testThing.create("Battlefield", "Dice", "PlayStation", "M");
+      //      testThing.create("Rust", "FacePunch", "PC", "M");
       testThing.readById(1);
-    } catch (SQLException e) {
+    }
+    catch (SQLException e)
+    {
       e.printStackTrace();
     }
 
