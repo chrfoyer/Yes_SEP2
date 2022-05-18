8f32702a1522ccbd4c21167d93deeca4f8e2ae0b
chrfoyer <chrfoyer@gmail.com>
2022-05-18 11:57:55 +0200

    Added DAO

---
+++ Server/src/Model/GameDAO.java
@@ -0,0 +1,16 @@
+package Model;
+
+import java.sql.SQLException;
+
+public interface GameDAO {
+
+  Game create(String name, String producer, String console, String esrb) throws SQLException;
+
+  Game create(Game game) throws SQLException;
+
+  Game readById(int id) throws SQLException;
+
+  void update(Game game) throws SQLException;
+
+  void delete(Game game) throws SQLException;
+}
