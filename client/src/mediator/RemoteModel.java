b3a5cb4bf74f050a414935950b074e3c2a81ee44
chrfoyer <chrfoyer@gmail.com>
2022-05-20 15:29:03 +0200

    Transaction reformat

--- client/src/mediator/RemoteModel.java
+++ client/src/mediator/RemoteModel.java
@@ -21,7 +21,7 @@ public interface RemoteModel extends Remote
 
   GameList viewGames() throws RemoteException;
 
-  GameList searchGames(String name, String console, String esrb) throws RemoteException;
+  ArrayList<Game> searchGames(String name, String console, String esrb) throws RemoteException;
 
   ArrayList<Game> getGamesRentedByUser(User user) throws SQLException, RemoteException;
 
