3c6b89b277e076123af4ebe4d41af43e92a34079
chrfoyer <chrfoyer@gmail.com>
2022-05-29 21:36:12 +0200

    Userlist add fix
    Throws an exception now if you try to add a null user in the user lsit

--- Server/src/Model/UserList.java
+++ Server/src/Model/UserList.java
@@ -31,197 +31,203 @@ public class UserList implements Serializable
      */
     public void addUser(User user)
     {
-        users.add(user);
+        if (user != null)
+        {
+            users.add(user);
+        } else
+        {
+            throw new IllegalArgumentException("You can't add an empty user");
+        }
     }
 
-    /**
-     * method for removing a user
-     *
-     * @param user user to be removed from the userList
-     */
-    public void removeUser(User user)
-    {
-        if (user.getUsername().equals("admin"))
+        /**
+         * method for removing a user
+         *
+         * @param user user to be removed from the userList
+         */
+        public void removeUser (User user)
         {
-            throw new IllegalArgumentException("wtf bro");
-        }
-        boolean found = false;
-        for (int i = 0; i < users.size(); i++)
-        {
-            if (users.get(i).getUsername().equals(user.getUsername()))
+            if (user.getUsername().equals("admin"))
             {
-                users.remove(i);
-                found = true;
+                throw new IllegalArgumentException("wtf bro");
             }
-        }
-        if (!found)
-        {
-            throw new IllegalArgumentException("User not found on server");
-        }
-    }
-
-    /**
-     * method for getting the size of the userList
-     *
-     * @return size of userList as an int
-     */
-    public int size()
-    {
-        return users.size();
-    }
-
-    /**
-     * method for getting a user at a certain index
-     *
-     * @param index index to fetch the user from
-     * @return user at the given index
-     */
-    public User get(int index)
-    {
-        return users.get(index);
-    }
-
-    /**
-     * Checks if a user is already registered
-     *
-     * @param given user to check against existing list
-     * @return true if it contains
-     */
-    public boolean contains(User given)
-    {
-        for (User user : users)
-        {
-            if (user.getUsername().equals(given.getUsername()))
-                return true;
-        }
-        return false;
-    }
-
-    /**
-     * User login with encryption
-     *
-     * @param username String username
-     * @param password String password
-     * @return boolean depending on if login was successful
-     * @throws Exception if something goes wrong with password encryption
-     */
-    public boolean login(String username, String password) throws Exception
-    {
-        User foundFromList = null;
-        for (User user : users
-        )
-        {
-            if (user.getUsername().equals(username))
+            boolean found = false;
+            for (int i = 0; i < users.size(); i++)
             {
-                foundFromList = user;
-                break;
+                if (users.get(i).getUsername().equals(user.getUsername()))
+                {
+                    users.remove(i);
+                    found = true;
+                }
+            }
+            if (!found)
+            {
+                throw new IllegalArgumentException("User not found on server");
             }
         }
 
-        if (foundFromList == null) throw new IllegalArgumentException("User does not exist on server");
-        String calculatedHash = "";
-
-        calculatedHash = PasswordEncryptor.getEncryptedPassword(password, foundFromList.getSalt());
-        System.out.println(calculatedHash);
-        if (calculatedHash.equals(foundFromList.getPassword())) return true;
-        throw new IllegalArgumentException("Password does not match stored credentials");
-    }
-
-    /**
-     * Finds a user within the user list.
-     *
-     * @param user The user to find
-     * @return The user if they are found
-     * @throws IllegalArgumentException Thrown when no user is found
-     */
-    public User findUserInList(User user)
-    {
-        for (User temp : users)
+        /**
+         * method for getting the size of the userList
+         *
+         * @return size of userList as an int
+         */
+        public int size ()
         {
-            if (temp.getUsername().equals(user.getUsername()))
-                return temp;
+            return users.size();
         }
-        throw new IllegalArgumentException("User does not exist on the server!");
-    }
 
-    /**
-     * Gets an array list with all the users
-     *
-     * @return All the users
-     */
-    public ArrayList<User> getUsers()
-    {
-        return users;
-    }
-
-    /**
-     * @param oldUser The old version of the user
-     * @param newUser The updated version of the user
-     * @throws IllegalArgumentException Thrown when the user cannot be found
-     * @deprecated Update the information of the user
-     */
-    public void updateUserInfo(User oldUser, User newUser)
-    {
-        boolean foundOld = false;
-        for (User user : users)
+        /**
+         * method for getting a user at a certain index
+         *
+         * @param index index to fetch the user from
+         * @return user at the given index
+         */
+        public User get ( int index)
         {
-            if (oldUser.equals(user))
+            return users.get(index);
+        }
+
+        /**
+         * Checks if a user is already registered
+         *
+         * @param given user to check against existing list
+         * @return true if it contains
+         */
+        public boolean contains (User given)
+        {
+            for (User user : users)
             {
-                user.setName(newUser.getName());
-                user.setUsername(newUser.getUsername());
-                // TODO: 2022. 05. 25. make this work lol 
-                user.changePassword(newUser.getPassword());
-                user.setEmail(newUser.getEmail());
-                user.setBday(newUser.getBday());
-                user.setAdmin(newUser.isAdmin());
-                user.setAddress(newUser.getAddress());
-                user.setHasSubscription(newUser.hasSubscription());
-                foundOld = true;
+                if (user.getUsername().equals(given.getUsername()))
+                    return true;
             }
+            return false;
         }
-        if (!foundOld)
-            throw new IllegalArgumentException(
-                    "No User found on server that could be updated");
-    }
 
-    /**
-     * Modifies the balance of a user
-     *
-     * @param ammount The amount to add or subtract
-     * @param user    The user to modify
-     */
-    public void modifyBalance(int ammount, User user)
-    {
-        user.modifyBalance(ammount);
-    }
+        /**
+         * User login with encryption
+         *
+         * @param username String username
+         * @param password String password
+         * @return boolean depending on if login was successful
+         * @throws Exception if something goes wrong with password encryption
+         */
+        public boolean login (String username, String password) throws Exception
+        {
+            User foundFromList = null;
+            for (User user : users
+            )
+            {
+                if (user.getUsername().equals(username))
+                {
+                    foundFromList = user;
+                    break;
+                }
+            }
 
-    /**
-     * Get the balance of the user
-     *
-     * @param user The user to retrieve the balance of
-     * @return The balance of the user
-     */
-    public int getBalance(User user)
-    {
-        return findUserInList(user).getBalance();
-    }
+            if (foundFromList == null) throw new IllegalArgumentException("User does not exist on server");
+            String calculatedHash = "";
 
-    /**
-     * Removes 30 balance from the user and set their subscription to active
-     *
-     * @param user The user to activate the subscription for
-     * @throws IllegalStateException    Thrown when the balance of the user is negative
-     * @throws IllegalArgumentException Thrown when the user has less than 30 balance
-     */
-    public void payForSubscription(User user)
-    {
-        if (user.getBalance() < 0)
-            throw new IllegalStateException(
-                    "Users with negative balance cant pay for a subscription");
-        if (user.getBalance() < 30)
-            throw new IllegalArgumentException(
-                    "Less than 30 money, add money to pay for subscription!");
-        user.modifyBalance(-30);
-        user.setHasSubscription(true);
+            calculatedHash = PasswordEncryptor.getEncryptedPassword(password, foundFromList.getSalt());
+            System.out.println(calculatedHash);
+            if (calculatedHash.equals(foundFromList.getPassword())) return true;
+            throw new IllegalArgumentException("Password does not match stored credentials");
+        }
+
+        /**
+         * Finds a user within the user list.
+         *
+         * @param user The user to find
+         * @return The user if they are found
+         * @throws IllegalArgumentException Thrown when no user is found
+         */
+        public User findUserInList (User user)
+        {
+            for (User temp : users)
+            {
+                if (temp.getUsername().equals(user.getUsername()))
+                    return temp;
+            }
+            throw new IllegalArgumentException("User does not exist on the server!");
+        }
+
+        /**
+         * Gets an array list with all the users
+         *
+         * @return All the users
+         */
+        public ArrayList<User> getUsers ()
+        {
+            return users;
+        }
+
+        /**
+         * @param oldUser The old version of the user
+         * @param newUser The updated version of the user
+         * @throws IllegalArgumentException Thrown when the user cannot be found
+         * @deprecated Update the information of the user
+         */
+        public void updateUserInfo (User oldUser, User newUser)
+        {
+            boolean foundOld = false;
+            for (User user : users)
+            {
+                if (oldUser.equals(user))
+                {
+                    user.setName(newUser.getName());
+                    user.setUsername(newUser.getUsername());
+                    // TODO: 2022. 05. 25. make this work lol
+                    user.changePassword(newUser.getPassword());
+                    user.setEmail(newUser.getEmail());
+                    user.setBday(newUser.getBday());
+                    user.setAdmin(newUser.isAdmin());
+                    user.setAddress(newUser.getAddress());
+                    user.setHasSubscription(newUser.hasSubscription());
+                    foundOld = true;
+                }
+            }
+            if (!foundOld)
+                throw new IllegalArgumentException(
+                        "No User found on server that could be updated");
+        }
+
+        /**
+         * Modifies the balance of a user
+         *
+         * @param ammount The amount to add or subtract
+         * @param user    The user to modify
+         */
+        public void modifyBalance ( int ammount, User user)
+        {
+            user.modifyBalance(ammount);
+        }
+
+        /**
+         * Get the balance of the user
+         *
+         * @param user The user to retrieve the balance of
+         * @return The balance of the user
+         */
+        public int getBalance (User user)
+        {
+            return findUserInList(user).getBalance();
+        }
+
+        /**
+         * Removes 30 balance from the user and set their subscription to active
+         *
+         * @param user The user to activate the subscription for
+         * @throws IllegalStateException    Thrown when the balance of the user is negative
+         * @throws IllegalArgumentException Thrown when the user has less than 30 balance
+         */
+        public void payForSubscription (User user)
+        {
+            if (user.getBalance() < 0)
+                throw new IllegalStateException(
+                        "Users with negative balance cant pay for a subscription");
+            if (user.getBalance() < 30)
+                throw new IllegalArgumentException(
+                        "Less than 30 money, add money to pay for subscription!");
+            user.modifyBalance(-30);
+            user.setHasSubscription(true);
+        }
     }
-}
